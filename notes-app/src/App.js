0167bd5ac5aea785afe7b4647a1a9bd52cf9e92f
boramenerja13 <boramenerja2000@gmail.com>
2024-10-03 17:57:55 +0200

    sidebar

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './App.css';
 import Navbar from '../src/components/navbar';
-// import Sidebar from './components/sidebar';
-// import CreateNote from './components/createNote';
+import Sidebar from './components/sidebar';
 import { Box, CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-      {/* Static Navbar */}
       <Navbar />
       <Box display="flex">
-        {/* Sidebar with adjustable margins */}
-        {/* <Sidebar /> */}
-        {/* Main Content for Notes */}
-        {/* <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <CreateNote />
-        </Box> */}
+        <Sidebar />
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, marginTop: '64px' }} 
+        ></Box>        
       </Box>
     </div>
   );
